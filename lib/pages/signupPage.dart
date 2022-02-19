4f522ee50c838a8ce7149d3c6300c02631f26b5e
AxelOliver <axeloliver300@gmail.com>
2022-02-19 23:03:59 +0800

    added max width and styles textfields

--- lib/pages/signupPage.dart
+++ lib/pages/signupPage.dart
@@ -9,20 +9,29 @@ class SignupPage extends StatefulWidget {
 
 class _SignupPageState extends State<SignupPage> {
   TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController reenterController = TextEditingController();
+
+  bool _isObscure = true;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('Login'),
-      ),
-      body: GestureDetector(
-        onTap: () {FocusScope.of(context).requestFocus(FocusNode());},
-        child: Container(
-          alignment: Alignment.center,
-          child: Column(
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: const Text('Login'),
+        ),
+        body: Center(
+          child: ListView(
             children: [
               Row(
                 children: [
@@ -50,80 +59,70 @@ class _SignupPageState extends State<SignupPage> {
               ),
               Center(
                 child: Padding(
-                  padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
-                  child: Row(
-                    children: [
-                      const Expanded(
-                        child: Text(
-                          'Username:',
-                          style: TextStyle(fontSize: 20,),
-                        ),
-                        flex: 3,
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: TextFormField(
-                            controller: usernameController,
-                            decoration: const InputDecoration(
-                              border: UnderlineInputBorder(),
-                              hintText: 'Enter Username',
-                            ),
+                  padding: const EdgeInsets.all(15),
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(maxWidth: 300),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: usernameController,
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Enter Username',
                           ),
                         ),
-                        flex: 6,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
-                  child: Row(
-                    children: [
-                      const Expanded(
-                        child: Text(
-                          'Password:',
-                          style: TextStyle(fontSize: 20,),
-                        ),
-                        flex: 3,
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: TextFormField(
-                            controller: usernameController,
-                            decoration: const InputDecoration(
-                              border: UnderlineInputBorder(),
-                              hintText: 'Enter Password',
-                            ),
+                        TextField(
+                          obscureText: _isObscure,
+                          controller: passwordController,
+                          decoration: InputDecoration(
+                              suffixIcon: IconButton(
+                                  icon: Icon(
+                                      _isObscure ? Icons.visibility : Icons.visibility_off),
+                                  onPressed: () {
+                                    setState(() {
+                                      _isObscure = !_isObscure;
+                                    });
+                                  }),
+                            border: const UnderlineInputBorder(),
+                            labelText: 'Enter Password',
                           ),
                         ),
-                        flex: 6,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                width: 200,
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: ElevatedButton(
-                    onPressed: () {print('login button pressed');},
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(fontSize: 18),
+                        TextField(
+                          obscureText: true,
+                          controller: reenterController,
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Re-enter Password',
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Center(
+                            child: ConstrainedBox(
+                              constraints: const BoxConstraints(minWidth: 200),
+                              child: Padding(
+                                padding: const EdgeInsets.all(12),
+                                child: ElevatedButton(
+                                  onPressed: () {print('signup button pressed');},
+                                  child: const Text(
+                                    'Sign up',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
-      ),
 
+      ),
     );
   }
 }
