7bb713263988c0d40106ce079736870a4b8f76de
Gianni5 <69453802+Gianni5@users.noreply.github.com>
2022-02-21 10:17:20 +0800

    login page finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tutorial_beginner/pages/loginPage.dart';
+import 'package:tutorial_beginner/pages/signupPage.dart';
 
 void main() => runApp(const MaterialApp(
   home: Home(),
 ));
 
-class Home extends StatelessWidget {
-  const Home({ Key? key }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Drastic on Plastic'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-      ),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-
-            Center(
-              
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter your username',
-                ),
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: const FloatingActionButton(
-          onPressed: null ,
-          child: Text('click'),
-          backgroundColor: Colors.green,
-
-        ),
-    );
-
-
-  }
-}
